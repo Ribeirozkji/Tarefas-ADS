f96e4f76eab1a59c1f83663aeb21904771bdee69
Ribeirozjki <phassumpcao246@gmail.com>
2025-04-29 09:00:57 -0300

    Add files via upload

---
+++ empregafácil/cadastro1.php
@@ -0,0 +1,29 @@
+<?php
+include("conexao.php");
+
+
+
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+
+
+
+$sql = "INSERT into empresa (email, senha)
+        VALUES ('$email', '$senha')";
+
+
+
+
+if (mysqli_query($conexao, $sql)) {
+   
+    header("Location: painel.php");
+    exit;
+} else {
+    echo "Erro: " . mysqli_error($conexao);
+}
+
+
+mysqli_close($conexao);
+?>
