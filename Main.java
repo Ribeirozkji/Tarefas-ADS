0db7778fb0b92fc3f35a79fb22b37391b3dc20ed
Ribeirozjki <phassumpcao246@gmail.com>
2025-05-15 10:40:06 -0300

    correção do código

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Scanner;
-// import org.junit.jupiter.api.Test;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
