357ee9515ffe1212ee03d6b9bf3c985ff44307a4
Ribeirozjki <phassumpcao246@gmail.com>
2025-05-20 08:30:36 -0300

    Create Atividade.java
    Atividade de Fixação em Java

---
+++ Atividade.java
@@ -0,0 +1,73 @@
+
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+
+//     //Questão 01)
+//     System.out.println("Escreva o primeiro número 1");
+//       double valor1 = scanner.nextDouble();
+
+//     System.out.println("Escreva o segundo número 2");
+//     double valor2 = scanner.nextDouble();
+      
+//    if (valor1 == valor2) {
+//     System.out.println("Número 1 é igual Número 2");
+// } if (valor1 != valor2) {
+//      System.out.println("Número 1 é diferente de Número 2");
+// }  if (valor1 > valor2){
+//      System.out.println("número 1 é maior que número 2");
+// } if(valor1 < valor2){
+//     System.out.println("número 1 é menor que número 2"); 
+// } if (valor1 >= valor2) {
+//      System.out.println("número 1 é maior ou igual a número 2");
+// }if (valor1 <= valor2) {
+//      System.out.println("número 1 é menor ou igual a número 2");
+// }
+    //Questão 03)
+  // System.out.println("Digite o código ");
+  // int CODIGO = scanner.nextInt();
+
+  //   switch (CODIGO){
+  //     case 1:
+  //       System.out.println("um");
+  //               break;
+  //           case 2:
+  //               System.out.println("dois");
+  //               break;
+  //           case 3:
+  //               System.out.println("três");
+  //               break;
+  //           default:
+  //               System.out.println("Código inválido.");
+  //       }
+          //Questão 03)
+    // System.out.println("Escreva o custo da fábrica do carro");
+    // double Custo_Fabrica = scanner.nextDouble();
+
+    // double distribuidor = Custo_Fabrica * 0.28;
+    // double impostos = Custo_Fabrica * 0.45;
+
+    // double Custo_final = Custo_Fabrica + distribuidor + impostos;
+    // System.out.println("O custo final ao consumidor é:" + Custo_final);
+  
+      //Questão 04)
+    //   ContaCorrente minhaConta = new ContaCorrente(100.0); 
+    //       minhaConta.depositar(50.0);
+    //       System.out.println("Saldo da conta: " + minhaConta.saldo);
+    //   }
+    // }
+    // class ContaCorrente {
+    //   double saldo;
+    //   public ContaCorrente(double saldoInicial) {
+    //     this.saldo = saldoInicial;
+    //   }
+    //   public void depositar(double valor) {
+    //     if (valor > 0) {
+    //       this.saldo += valor;
+    //     } else {
+    //       System.out.println("Valor inválido para depósito.");
+    //     }
+  
+  
